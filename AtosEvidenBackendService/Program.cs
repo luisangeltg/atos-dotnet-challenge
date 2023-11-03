c54bc0defe9eacf5ea910833ca523e571f3d803e
Luis Angel Torres Grimaldo <t.g.luisangel@hotmail.com>
2023-11-03 16:15:19 -0600

    first commit

--- AtosEvidenBackendService/Program.cs
+++ AtosEvidenBackendService/Program.cs
@@ -1,28 +1,29 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿using AtosEvidenBackendService.Controllers;
+using Microsoft.AspNetCore.Mvc;
 
-// Add services to the container.
-builder.Services.AddControllersWithViews();
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowLocalhost4200",
+        builder =>
+        {
+            builder.WithOrigins("http://localhost:4200")
+                   .AllowAnyHeader()
+                   .AllowAnyMethod();
+        });
+});
 
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
-
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-
 app.UseRouting();
+app.UseCors("AllowLocalhost4200");
 
-app.UseAuthorization();
 
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+app.MapGet("/allPokemons", () => {
+    var pokemons = UserRepositoryController.pokemons;
+
+    var jsonResponse = new OkObjectResult(pokemons);
+    return jsonResponse;
+});
 
 app.Run();
 
